ca1326573c13250c73a81eb8d587e95d4e9ae1c7
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-06 11:10:13 +0700

    day5 dirty

---
+++ day5/part1.py
@@ -0,0 +1,22 @@
+def __main__():
+    maxseat = 0
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            boarding_pass = line.strip()
+            row, seat = boarding_pass[:7], boarding_pass[7:]
+            count = 0
+            for i, char in enumerate(reversed(row)):
+                if char == 'B':
+                    count += 2 ** i
+            count *= 8
+            for i, char in enumerate(reversed(seat)):
+                if char == 'R':
+                    count += 2 ** i
+            if count > maxseat:
+                maxseat = count
+    return maxseat
+
+
+if __name__ == "__main__":
+    print(__main__())
+
---
+++ day5/part2.py
@@ -0,0 +1,27 @@
+def __main__():
+    maxseat = 0
+    seats = set()
+    with open("input.txt", "r") as f:
+        for line in f.readlines():
+            boarding_pass = line.strip()
+            row, seat = boarding_pass[:7], boarding_pass[7:]
+            count = 0
+            for i, char in enumerate(reversed(row)):
+                if char == 'B':
+                    count += 2 ** i
+            count *= 8
+            for i, char in enumerate(reversed(seat)):
+                if char == 'R':
+                    count += 2 ** i
+            seats.add(count)
+    seats = list(seats)
+    prev = seats[0]
+    for seat in seats[1:]:
+        if seat - 1 != prev:
+            return seat - 1
+        prev = seat
+
+
+if __name__ == "__main__":
+    print(__main__())
+
