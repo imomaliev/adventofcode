6e62341315190a337d860531ef8fdad756b17219
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-10 22:27:08 +0700

    test for day02

--- day02/part1.py
+++ day02/part1.py
@@ -1,18 +1,40 @@
+import sys
 from collections import Counter
+import pytest
 
 
-def __main__():
+def solve(input_s):
     correct = 0
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            rule, password = line.split(": ")
-            num, char = rule.split(" ")
-            bot, top = [int(i) for i in num.split("-")]
-            counter = Counter(password.strip())
-            if bot <= counter[char] <= top:
-                correct += 1
+    for line in input_s.splitlines():
+        rule, password = line.split(": ")
+        num, char = rule.split(" ")
+        bot, top = [int(i) for i in num.split("-")]
+        counter = Counter(password.strip())
+        if bot <= counter[char] <= top:
+            correct += 1
     return correct
 
 
+INPUT_S = """\
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 2),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
+
+
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day02/part2.py
+++ day02/part2.py
@@ -1,17 +1,41 @@
-def __main__():
+import sys
+import pytest
+
+
+def solve(input_s):
     correct = 0
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            rule, password = line.split(": ")
-            num, char = rule.split(" ")
-            bot, top = [int(i) - 1 for i in num.split("-")]
-            password = password.strip()
-            if (password[top] == char and password[bot] != char) or (
-                password[bot] == char and password[top] != char
-            ):
-                correct += 1
+    for line in input_s.splitlines():
+        rule, password = line.split(": ")
+        num, char = rule.split(" ")
+        bot, top = [int(i) - 1 for i in num.split("-")]
+        password = password.strip()
+        if (password[top] == char and password[bot] != char) or (
+            password[bot] == char and password[top] != char
+        ):
+            correct += 1
     return correct
 
 
+INPUT_S = """\
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 1),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
+
+
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
