1ca6aaddee2856c8a8e5c04a2042ca14eb6dcb88
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-10 17:10:26 +0700

    day10 working

--- day10/part2.py
+++ day10/part2.py
@@ -1,58 +1,39 @@
 # pylint: disable=too-few-public-methods
-class Node:
-    def __init__(self, val, left, mid=None, right=None):
-        self.val = val
-        self.left = left
-        self.mid = mid
-        self.right = right
-
-    def __repr__(self):
-        return f"<Node({self.val})>"
 
 
-def fill_tree(node, index, numbers):
-    if index >= len(numbers):
-        return
-    node.left = Node(numbers[index], None, None, None)
-    fill_tree(node.left, index + 1, numbers)
-    if numbers[index] - node.val < 3:
-        if (
-            (index + 2 < len(numbers))
-            and (numbers[index + 1] - node.val <= 3)
-            and (numbers[index + 2] - numbers[index + 1] <= 3)
-        ):
-            node.mid = Node(numbers[index + 1], None, None, None)
-            fill_tree(node.mid, index + 2, numbers)
-
-        if (index + 3 < len(numbers)) and (numbers[index + 2] - node.val <= 3):
-            node.right = Node(numbers[index + 3], None, None, None)
-            fill_tree(node.right, index + 4, numbers)
-    return
+def get_for_n(num):
+    if num == 1:
+        return 1
+    if num == 2:
+        return 2
+    if num == 3:
+        return 4
+    return get_for_n(num - 1) + get_for_n(num - 2) + get_for_n(num - 3)
 
 
 def __main__():
-    with open("input2.txt", "r") as f:
+    with open("input.txt", "r") as f:
         numbers = []
         for line in f.readlines():
             num = int(line.strip())
             numbers.append(num)
         numbers = sorted(numbers)
         numbers.append(numbers[-1] + 3)
-        root = curr = Node(0, None, None, None)
-        fill_tree(root, 0, numbers)
-        nodes = [curr]
+        diffs = []
+        prev = 0
+        for num in numbers:
+            diffs.append(num - prev)
+            prev = num
+        res = 1
         counter = 0
-        while nodes:
-            node = nodes.pop(0)
-            if node.left:
-                nodes.append(node.left)
-            if node.mid:
-                nodes.append(node.mid)
-            if node.right:
-                nodes.append(node.right)
-            if node.left is None and node.mid is None and node.right is None:
+        for diff in diffs:
+            if diff == 1:
                 counter += 1
-        return counter
+            else:
+                if counter:
+                    res *= get_for_n(counter)
+                counter = 0
+        return res
     raise RuntimeError
 
 
