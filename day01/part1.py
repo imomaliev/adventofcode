4b4823a7a4f695a239ce611ae86f9bf3358df8e5
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-10 22:19:08 +0700

    use pytest to check results, small cleanup day01

--- day01/part1.py
+++ day01/part1.py
@@ -1,14 +1,41 @@
-def __main__():
+import sys
+import pytest
+
+
+def solve(input_s):
     expenses = []
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            curr = int(line)
-            for expense in expenses:
-                if curr + expense == 2020:
-                    return curr * expense
-            expenses.append(curr)
-    return None
+    for line in input_s.splitlines():
+        curr = int(line)
+        for expense in expenses:
+            if curr + expense == 2020:
+                return curr * expense
+        expenses.append(curr)
+    return NotImplementedError
+
+
+INPUT_S = """\
+1721
+979
+366
+299
+675
+1456
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 514579),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day01/part2.py
+++ day01/part2.py
@@ -1,16 +1,43 @@
-def __main__():
+import sys
+import pytest
+
+
+def solve(input_s):
     expenses = []
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            curr = int(line)
-            for i, expense1 in enumerate(expenses):
-                for j, expense2 in enumerate(expenses):
-                    if i != j:
-                        if curr + expense1 + expense2 == 2020:
-                            return curr * expense1 * expense2
-            expenses.append(curr)
-    return None
+    for line in input_s.splitlines():
+        curr = int(line)
+        for i, expense1 in enumerate(expenses):
+            for j, expense2 in enumerate(expenses):
+                if i != j:
+                    if curr + expense1 + expense2 == 2020:
+                        return curr * expense1 * expense2
+        expenses.append(curr)
+    return NotImplementedError
+
+
+INPUT_S = """\
+1721
+979
+366
+299
+675
+1456
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 241861950),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
