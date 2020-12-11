d018b56935cea41fae52e7900b83a15fd1901fd9
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-11 09:01:25 +0700

    test for day10

--- day10/part1.py
+++ day10/part1.py
@@ -1,23 +1,90 @@
+import sys
 from collections import Counter
+import pytest
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        numbers = []
-        for line in f.readlines():
-            num = int(line.strip())
-            numbers.append(num)
-        numbers = sorted(numbers)
-        counter = Counter()
-        prev = 0
-        counter[3] += 1
-        for curr in numbers:
-            counter[curr - prev] += 1
-            prev = curr
+def solve(input_s):
+    numbers = []
+    for line in input_s.splitlines():
+        num = int(line.strip())
+        numbers.append(num)
+    numbers = sorted(numbers)
+    counter = Counter()
+    prev = 0
+    counter[3] += 1
+    for curr in numbers:
+        counter[curr - prev] += 1
+        prev = curr
 
-        return counter[1] * counter[3]
-    raise RuntimeError
+    return counter[1] * counter[3]
+
+
+INPUT_S = """\
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4
+"""
+
+
+INPUT_S2 = """\
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        (INPUT_S, 35),
+        (INPUT_S2, 220),
+    ),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day10/part2.py
+++ day10/part2.py
@@ -1,4 +1,5 @@
-# pylint: disable=too-few-public-methods
+import sys
+import pytest
 
 
 def get_for_n(num):
@@ -11,31 +12,96 @@ def get_for_n(num):
     return get_for_n(num - 1) + get_for_n(num - 2) + get_for_n(num - 3)
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        numbers = []
-        for line in f.readlines():
-            num = int(line.strip())
-            numbers.append(num)
-        numbers = sorted(numbers)
-        numbers.append(numbers[-1] + 3)
-        diffs = []
-        prev = 0
-        for num in numbers:
-            diffs.append(num - prev)
-            prev = num
-        res = 1
-        counter = 0
-        for diff in diffs:
-            if diff == 1:
-                counter += 1
-            else:
-                if counter:
-                    res *= get_for_n(counter)
-                counter = 0
-        return res
-    raise RuntimeError
+def solve(input_s):
+    numbers = []
+    for line in input_s.splitlines():
+        num = int(line.strip())
+        numbers.append(num)
+    numbers = sorted(numbers)
+    numbers.append(numbers[-1] + 3)
+    diffs = []
+    prev = 0
+    for num in numbers:
+        diffs.append(num - prev)
+        prev = num
+    res = 1
+    counter = 0
+    for diff in diffs:
+        if diff == 1:
+            counter += 1
+        else:
+            if counter:
+                res *= get_for_n(counter)
+            counter = 0
+    return res
+
+
+INPUT_S = """\
+16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4
+"""
+
+
+INPUT_S2 = """\
+28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        (INPUT_S, 8),
+        (INPUT_S2, 19208),
+    ),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
