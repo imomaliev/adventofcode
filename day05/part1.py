9b756416617dedd3f907ad888c5367c580684d7a
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-11 08:23:52 +0700

    test for day05

--- day05/part1.py
+++ day05/part1.py
@@ -1,21 +1,45 @@
-def __main__():
+import sys
+import pytest
+
+
+def solve(input_s):
     maxseat = 0
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            boarding_pass = line.strip()
-            row, seat = boarding_pass[:7], boarding_pass[7:]
-            count = 0
-            for i, char in enumerate(reversed(row)):
-                if char == "B":
-                    count += 2 ** i
-            count *= 8
-            for i, char in enumerate(reversed(seat)):
-                if char == "R":
-                    count += 2 ** i
-            if count > maxseat:
-                maxseat = count
+    for line in input_s.splitlines():
+        boarding_pass = line.strip()
+        row, seat = boarding_pass[:7], boarding_pass[7:]
+        count = 0
+        for i, char in enumerate(reversed(row)):
+            if char == "B":
+                count += 2 ** i
+        count *= 8
+        for i, char in enumerate(reversed(seat)):
+            if char == "R":
+                count += 2 ** i
+        if count > maxseat:
+            maxseat = count
     return maxseat
 
 
+INPUT_S = """\
+BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 820),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
+
+
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day05/part2.py
+++ day05/part2.py
@@ -1,24 +1,58 @@
-def __main__():
+import sys
+import pytest
+
+
+def solve(input_s):
     seats = set()
-    with open("input.txt", "r") as f:
-        for line in f.readlines():
-            boarding_pass = line.strip()
-            row, seat = boarding_pass[:7], boarding_pass[7:]
-            count = 0
-            for i, char in enumerate(reversed(row)):
-                if char == "B":
-                    count += 2 ** i
-            count *= 8
-            for i, char in enumerate(reversed(seat)):
-                if char == "R":
-                    count += 2 ** i
-            seats.add(count)
+    for line in input_s.splitlines():
+        boarding_pass = line.strip()
+        row, seat = boarding_pass[:7], boarding_pass[7:]
+        count = 0
+        for i, char in enumerate(reversed(row)):
+            if char == "B":
+                count += 2 ** i
+        count *= 8
+        for i, char in enumerate(reversed(seat)):
+            if char == "R":
+                count += 2 ** i
+        seats.add(count)
     seats = list(seats)
     prev = seats[0]
     for seat in seats[1:]:
         if seat - 1 != prev:
             return seat - 1
         prev = seat
+    return NotImplementedError
+
+
+INPUT_S = """\
+FFFBBBFRLR
+FFFBBBFRRL
+FFFBBBFRRR
+FFFBBBBLLR
+FFFBBBBLRL
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 120),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+def __main__():
     raise RuntimeError
 
 
