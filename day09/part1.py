bc5f18896a262e493c7900bcbb3a60b3d70992ca
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-11 08:53:37 +0700

    test for day09

--- day09/part1.py
+++ day09/part1.py
@@ -1,25 +1,64 @@
+import sys
 from itertools import combinations
+import pytest
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        numbers = []
-        sub = []
-        for line in f.readlines():
-            num = int(line.strip())
-            numbers.append(num)
-            if len(sub) < 25:
-                sub.append(num)
+def solve(input_s, preamble=25):
+    numbers = []
+    sub = []
+    for line in input_s.splitlines():
+        num = int(line.strip())
+        numbers.append(num)
+        if len(sub) < preamble:
+            sub.append(num)
+        else:
+            for i, j in combinations(sub, 2):
+                if i + j == num:
+                    sub.pop(0)
+                    sub.append(num)
+                    break
             else:
-                for i, j in combinations(sub, 2):
-                    if i + j == num:
-                        sub.pop(0)
-                        sub.append(num)
-                        break
-                else:
-                    return num
-    raise RuntimeError
+                return num
+    raise NotImplementedError
+
+
+INPUT_S = """\
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 127),),
+)
+def test(input_s, expected):
+    assert solve(input_s, 5) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day09/part2.py
+++ day09/part2.py
@@ -1,24 +1,63 @@
-NUM = 14144619
+import sys
+import pytest
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        numbers = []
-        sub = []
-        for line in f.readlines():
-            num = int(line.strip())
-            numbers.append(num)
+def solve(input_s, cnum=14144619):
+    numbers = []
+    sub = []
+    for line in input_s.splitlines():
+        num = int(line.strip())
+        numbers.append(num)
 
-        start, end = 0, 2
-        while end < len(numbers):
-            sub = numbers[start:end]
-            if sum(sub) == NUM:
-                return min(sub) + max(sub)
-            elif sum(sub) < NUM:
-                end += 1
-            else:
-                start += 1
+    start, end = 0, 2
+    while end < len(numbers):
+        sub = numbers[start:end]
+        if sum(sub) == cnum:
+            return min(sub) + max(sub)
+        elif sum(sub) < cnum:
+            end += 1
+        else:
+            start += 1
+    raise NotImplementedError
+
+
+INPUT_S = """\
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 62),),
+)
+def test(input_s, expected):
+    assert solve(input_s, 127) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
