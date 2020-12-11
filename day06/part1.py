065983e0f2cb8bba28c66023fd2d82e49f2c8b5a
Sardorbek Imomaliev <sardorbek.imomaliev@gmail.com>
2020-12-11 08:33:36 +0700

    test for day06

--- day06/part1.py
+++ day06/part1.py
@@ -1,17 +1,50 @@
+import sys
 from collections import Counter
+import pytest
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        input_file = f.read()
-        counter = Counter()
-        res = 0
-        for group in input_file.split("\n\n"):
-            group = group.replace("\n", "")
-            counter = Counter(group)
-            res += len(counter.keys())
-        return res
+def solve(input_s):
+    counter = Counter()
+    res = 0
+    for group in input_s.split("\n\n"):
+        group = group.replace("\n", "")
+        counter = Counter(group)
+        res += len(counter.keys())
+    return res
+
+
+INPUT_S = """\
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 11),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
--- day06/part2.py
+++ day06/part2.py
@@ -1,18 +1,51 @@
+import sys
 from collections import Counter
+import pytest
 
 
-def __main__():
-    with open("input.txt", "r") as f:
-        input_file = f.read()
-        res = 0
-        for group in input_file.split("\n\n"):
-            first, *rest = group.split("\n")
-            keys = set(first)
-            for subgroup in rest:
-                keys &= set(Counter(subgroup))
-            res += len(keys)
-        return res
+def solve(input_s):
+    res = 0
+    for group in input_s.split("\n\n"):
+        first, *rest = group.strip().split("\n")
+        keys = set(first)
+        for subgroup in rest:
+            keys &= set(Counter(subgroup))
+        res += len(keys)
+    return res
+
+
+INPUT_S = """\
+abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 6),),
+)
+def test(input_s, expected):
+    assert solve(input_s) == expected
+
+
+def main():
+    with open("input.txt") as f:
+        print(solve(f.read()))
+    return 0
 
 
 if __name__ == "__main__":
-    print(__main__())
+    sys.exit(main())
