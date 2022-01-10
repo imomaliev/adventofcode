7b436b8ddbb9a71e955da12c1bf4e7fc9f6e99b7
Sardorbek Imomaliev <imomaliev@hey.com>
2022-01-10 18:32:51 +0700

    install eslint-config-prettier
    Read more: https://dev.to/imomaliev/make-eslint-and-prettier-play-nice-together-38ab

--- 2021/typescript/.eslintrc.js
+++ 2021/typescript/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   root: true,
   env: {
-        node: true,
+    node: true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-};
+}
