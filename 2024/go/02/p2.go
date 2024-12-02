14be3bc31d671cdd8d3da0e04e0a89dded5b2058
Sardorbek Imomaliev <imomaliev@hey.com>
2024-12-02 23:09:17 +0000

    2024: WIP day2 part2

--- 2024/go/02/p2.go
+++ 2024/go/02/p2.go
@@ -6,27 +6,82 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
+func AbsInt(x int) int {
+	if x > 0 {
+		return x
+	}
+	return -x
+}
+
+func getDirection(b bool) int {
+	if b {
+		return 1
+	}
+	return -1
+}
+
 func Solve(scanner *bufio.Scanner) int {
-	var input []int
 	var err error
+	var result = 0
+
 	for scanner.Scan() {
-		value, err := strconv.Atoi(scanner.Text())
+		values := strings.Split(scanner.Text(), " ")
+		prev, err := strconv.Atoi(values[0])
 		if err != nil {
 			log.Fatal(err)
 		}
-		input = append(input, value)
+		curr, err := strconv.Atoi(values[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		direction := getDirection(curr > prev)
+		total := len(values)
+
+		diff := AbsInt(curr - prev)
+		if diff < 1 || diff > 3 {
+			continue
+		}
+
+		prev = curr
+		nobreak := true
+		fmt.Println(direction)
+		for _, val := range values[2:] {
+			curr, err := strconv.Atoi(val)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			direction += getDirection(curr > prev)
+			fmt.Println(direction)
+			diff = AbsInt(curr - prev)
+			if diff < 1 || diff > 3 {
+				nobreak = false
+				break
+			}
+			prev = curr
+		}
+		absDirection := AbsInt(direction)
+		fmt.Println(values)
+		fmt.Println(nobreak, direction, absDirection)
+		if nobreak && absDirection >= total-3 {
+			fmt.Println("safe")
+			result += 1
+		}
 	}
 
 	if err = scanner.Err(); err != nil {
 		log.Fatalf("Scanner error: %v", err)
 	}
+	return result
 }
 
 func main() {
 	log.SetPrefix("day00:part2: ")
-	file, err := os.Open("../../input/00.txt")
+	file, err := os.Open("../../input/02.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
--- 2024/go/02/p2_test.go
+++ 2024/go/02/p2_test.go
@@ -8,13 +8,19 @@ import (
 )
 
 var INPUT_S string = `
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 `
 
 var tests = []struct {
 	input    string
 	expected int
 }{
-	{INPUT_S, ...},
+	{INPUT_S, 4},
 }
 
 // TestHelloName calls greetings.Hello with a name, checking
