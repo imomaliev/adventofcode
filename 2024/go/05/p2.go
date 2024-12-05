0118ff814a1c7aa0f5d207019d55855817cfe1c8
Sardorbek Imomaliev <imomaliev@hey.com>
2024-12-05 21:37:22 +0000

    2024: day05 part2 WIP

--- 2024/go/05/p2.go
+++ 2024/go/05/p2.go
@@ -5,28 +5,124 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
+	"strings"
 )
 
-func Solve(scanner *bufio.Scanner) int {
-	var input []int
-	var err error
-	for scanner.Scan() {
-		value, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			log.Fatal(err)
+func FixOrder(rules map[int][]int, update []int) []int {
+	var ejected []int
+	var correct []int
+	for _, val := range update {
+		correct = append(correct, val)
+	}
+	for i, page := range update {
+		for j := 0; j < len(update); j++ {
+			if j < i && !slices.Contains(rules[update[j]], page) {
+				if !slices.Contains(ejected, page) {
+					ejected = append(ejected, page)
+				}
+				var newCorrect []int
+				for _, val := range correct {
+					if val != page {
+						newCorrect = append(newCorrect, val)
+					}
+				}
+				correct = newCorrect
+				break
+			}
+			if j > i && !slices.Contains(rules[page], update[j]) {
+				if !slices.Contains(ejected, page) {
+					ejected = append(ejected, page)
+				}
+				var newCorrect []int
+				for _, val := range correct {
+					if val != page {
+						newCorrect = append(newCorrect, val)
+					}
+				}
+				correct = newCorrect
+				break
+			}
+			// if !slices.Contains(correct, page) {
+			// 	correct = append(correct, page)
+			// }
 		}
-		input = append(input, value)
+	}
+	fmt.Println(update, correct, ejected)
+	return correct
+}
+
+func Solve(scanner *bufio.Scanner) int {
+	var err error
+	var rules = make(map[int][]int)
+	rulesScanned := false
+	var updates [][]int
+	result := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			rulesScanned = true
+			continue
+		}
+
+		if !rulesScanned {
+			values := strings.Split(line, "|")
+			key, err := strconv.Atoi(values[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			val, err := strconv.Atoi(values[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			rules[key] = append(rules[key], val)
+		} else {
+			values := strings.Split(line, ",")
+			var update []int
+			for _, val := range values {
+				page, err := strconv.Atoi(val)
+				if err != nil {
+					log.Fatal(err)
+				}
+				update = append(update, page)
+			}
+			updates = append(updates, update)
+		}
+
+	}
+
+	for _, update := range updates {
+		correct := true
+		for i, page := range update {
+			for j := 0; j < len(update); j++ {
+				if j < i && !slices.Contains(rules[update[j]], page) {
+					correct = false
+					break
+				}
+				if j > i && !slices.Contains(rules[page], update[j]) {
+					correct = false
+					break
+				}
+			}
+		}
+		if !correct {
+			update = FixOrder(rules, update)
+			// result += update[len(update)/2]
+		}
 	}
 
 	if err = scanner.Err(); err != nil {
 		log.Fatalf("Scanner error: %v", err)
 	}
+	return result
 }
 
 func main() {
-	log.SetPrefix("day00:part2: ")
-	file, err := os.Open("../../input/00.txt")
+	log.SetPrefix("day00:part1: ")
+	file, err := os.Open("../../input/05.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
--- 2024/go/05/p2_test.go
+++ 2024/go/05/p2_test.go
@@ -8,13 +8,41 @@ import (
 )
 
 var INPUT_S string = `
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
 `
 
 var tests = []struct {
 	input    string
 	expected int
 }{
-	{INPUT_S, ...},
+	{INPUT_S, 123},
 }
 
 // TestHelloName calls greetings.Hello with a name, checking
